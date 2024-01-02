4d9421c97e1fe36f848528b7959dfa2d9ef9a3ed
vijeesh <vijeeshkumbalath@gmail.com>
2024-01-02 16:09:55 +0530

    Initial Commit

---
+++ OMDB/OMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  OMDBApp.swift
+//  OMDB
+//
+//  Created by vijeesh k on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct OMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
